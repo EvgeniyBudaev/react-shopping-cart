847d1f88d0dc9fb70c25cadf3c1f180c7e375673
EvgeniyBudaev <budaev.e@gmail.com>
2020-08-09 17:19:50 +0300

    products backend

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react'
 import formatCurrency from '../utils'
+import Fade from 'react-reveal/Fade'
+import Modal from 'react-modal'
+import Zoom from 'react-reveal/Zoom'
 
 export default class Products extends Component {
+  state = {product: null}
+
+  openModal = product => {
+    this.setState({
+      product
+    })
+  }
+
+  closeModal = () => {
+    this.setState({
+      product: null
+    })
+  }
+
   render() {
+    const {product} = this.state
     return (
       <div>
+        <Fade bottom cascade>
         <ul className="products">
           {this.props.products.map(product => (
             <li key={product._id}>
               <div className="product">
-                <a href={"#" + product.id}>
+                <a href={"#" + product.id} onClick={() => this.openModal(product)}>
                   <img src={product.image} alt={product.title} />
                   <p>
                     {product.title}
@@ -28,6 +47,52 @@ export default class Products extends Component {
             </li>
           ))}
         </ul>
+        </Fade>
+        {
+          product && (
+            <Modal 
+            isOpen={true}
+            onRequestClose={this.closeModal}
+            >
+            <Zoom>
+              <button className="close-modal" onClick={this.closeModal}>
+                x
+                </button>
+              <div className="product-details">
+                <img src={product.image} alt={product.title}></img>
+                <div className="product-details0description">
+                  <p>
+                    <strong>{product.title}</strong>
+                  </p>
+                  <p>
+                    {product.description}
+                  </p>
+                  <p>
+                  Avaiable Sizes:{" "}
+                  {product.availableSizes.map(x => (
+  <span>{" "}<button className="button">{x}</button></span>
+                  ))}
+                  </p>
+                  <div className="produc-price">
+                    <div>
+                    {formatCurrency(product.price)}
+                    </div>
+                    <button
+                     className="button primary" onClick={() => {
+                      this.props.addToCart(product)
+                      this.closeModal()
+                     }
+                    }
+                     >
+                       Add to cart
+                     </button>
+                  </div>
+                </div>
+              </div>
+            </Zoom>
+          </Modal>
+          )
+        }
       </div>
     )
   }
